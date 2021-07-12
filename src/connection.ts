a4d133cd25905c8c3a74f459d3814874bec2b9f1
Donovan Lambert <d.lambert@eona.com>
2021-07-12 11:06:07 +0200

    Refactoring

--- src/connection.ts
+++ src/connection.ts
@@ -41,70 +41,83 @@ const createIdGenerators = (): IdGenerators => ({
 });
 
 class Connection {
-    public sessionId: number | null = null;
+    private readonly _options: ConnectionOptions;
 
-    private transport: TransportInterface | null = null;
+    protected _sessionId: number | null = null;
+
+    private _transport: TransportInterface | null = null;
 
     // - The type of processors has to match the order of the factories in processorFactories.
-    private processors: [Publisher, Subscriber, Caller, Callee] | null = null;
-    private processorsFactories: ProcessorFactoryInterface[] = [Publisher, Subscriber, Caller, Callee];
+    private _processors: [Publisher, Subscriber, Caller, Callee] | null = null;
+    private _processorsFactories: ProcessorFactoryInterface[] = [Publisher, Subscriber, Caller, Callee];
 
-    private idGenerators: IdGenerators;
-    private state: ConnectionStateMachine;
+    private _idGenerators: IdGenerators;
+    private _state: ConnectionStateMachine;
 
-    private serializer: SerializerInterface;
-    private authProvider: AuthProviderInterface | null;
+    private _serializer: SerializerInterface;
+    private _authProvider: AuthProviderInterface | null;
 
-    private openedDeferred: Deferred<WelcomeDetails> | null = null;
-    private closedDeferred: Deferred<ConnectionCloseInfo> | null = null;
+    private _openedDeferred: Deferred<WelcomeDetails> | null = null;
+    private _closedDeferred: Deferred<ConnectionCloseInfo> | null = null;
 
-    private readonly logger: Logger;
+    private readonly _logger: Logger;
 
-    public get closed(): Promise<ConnectionCloseInfo> {
-        if (!this.closedDeferred) {
-            this.closedDeferred = new Deferred();
-        }
-        return this.closedDeferred.promise;
+    public get sessionId(): number | null {
+        return this._sessionId;
     }
 
-    constructor(private connectionOptions: ConnectionOptions) {
-        this.state = new ConnectionStateMachine();
-        this.idGenerators = createIdGenerators();
-        this.serializer = this.connectionOptions?.serializer ?? new JSONSerializer();
-        this.authProvider = this.connectionOptions?.authProvider ?? null;
-        this.logger = new Logger(connectionOptions.logFunction, !!connectionOptions.debug);
+    public get isRetrying(): boolean {
+        return this._isRetrying;
+    }
+
+    public get closed(): Promise<ConnectionCloseInfo> {
+        if (!this._closedDeferred) {
+            this._closedDeferred = new Deferred();
+        }
+        return this._closedDeferred.promise;
+    }
+
+    constructor(options: ConnectionOptions) {
+        this._options = options;
+        this._serializer = this._options?.serializer ?? new JSONSerializer();
+        this._authProvider = this._options?.authProvider ?? null;
+
+        this._state = new ConnectionStateMachine();
+        this._idGenerators = createIdGenerators();
+
+        this._logger = new Logger(options.logFunction, !!options.debug);
     }
 
     public open(): Promise<WelcomeDetails> {
-        if (this.transport) {
+        if (this._transport) {
             return Promise.reject('Transport already opened or opening.');
         }
 
-        this.transport = new WebSocketTransport(this.serializer);
-        this.state = new ConnectionStateMachine();
-        this.openedDeferred = new Deferred();
-        this.transport.open(
-            this.connectionOptions.endpoint,
+        this._transport = new WebSocketTransport(this._serializer);
+        this._state = new ConnectionStateMachine();
+        this._openedDeferred = new Deferred();
+        this._transport.open(
+            this._options.endpoint,
             this.handleTransportEvent.bind(this),
         );
 
-        this.logger.log(LogLevel.DEBUG, 'Connection opened.');
-        return this.openedDeferred.promise;
+        this._logger.log(LogLevel.DEBUG, 'Connection opened.');
+        return this._openedDeferred.promise;
     }
 
     public close(): Promise<ConnectionCloseInfo> {
-        if (!this.transport) {
-            return Promise.reject('Transport is not open.');
+        if (!this._transport) {
+            return Promise.reject('Connection already closed.');
         }
 
-        this.transport.send([
+        this._transport.send([
             EWampMessageID.GOODBYE,
             { message: 'client shutdown' },
             'wamp.close.normal',
         ]);
 
-        this.logger.log(LogLevel.DEBUG, 'Closing Connection.');
-        this.state.update([EMessageDirection.SENT, EWampMessageID.GOODBYE]);
+        this._logger.log(LogLevel.DEBUG, 'Closing Connection.');
+        this._state.update([EMessageDirection.SENT, EWampMessageID.GOODBYE]);
 
         return this.closed;
     }
@@ -119,13 +132,13 @@ class Connection {
         kwargs?: K,
         opts?: CallOptions,
     ): CallReturn<RA, RK> {
-        if (!this.processors) {
+        if (!this._processors) {
             return [
                 Promise.reject('Invalid session state.'),
                 () => Promise.resolve(),
             ];
         }
-        return this.processors[2].call(uri, args, kwargs, opts);
+        return this._processors[2].call(uri, args, kwargs, opts);
     }
 
     public register<A extends WampList, K extends WampDict, RA extends WampList, RK extends WampDict>(
@@ -133,10 +146,10 @@ class Connection {
         handler: CallHandler<A, K, RA, RK>,
         opts?: RegisterOptions,
     ): Promise<Registration> {
-        if (!this.processors) {
+        if (!this._processors) {
             return Promise.reject('Invalid session state.');
         }
-        return this.processors[3].register(uri, handler, opts);
+        return this._processors[3].register(uri, handler, opts);
     }
 
     public subscribe<A extends WampList, K extends WampDict>(
@@ -144,10 +157,10 @@ class Connection {
         handler: EventHandler<A, K>,
         opts?: SubscribeOptions,
     ): Promise<Subscription> {
-        if (!this.processors) {
+        if (!this._processors) {
             return Promise.reject('Invalid session state.');
         }
-        return this.processors[1].subscribe(uri, handler, opts);
+        return this._processors[1].subscribe(uri, handler, opts);
     }
 
     public publish<A extends WampList, K extends WampDict>(
@@ -156,10 +169,10 @@ class Connection {
         kwargs?: K,
         opts?: PublishOptions,
     ): Promise<Publication> {
-        if (!this.processors) {
+        if (!this._processors) {
             return Promise.reject('Invalid session state.');
         }
-        return this.processors[0].publish(uri, args, kwargs, opts);
+        return this._processors[0].publish(uri, args, kwargs, opts);
     }
 
     //
@@ -167,79 +180,79 @@ class Connection {
     //
 
     private processSessionMessage(msg: WampMessage): void {
-        if (!this.transport) {
+        if (!this._transport) {
             return;
         }
 
-        this.state.update([EMessageDirection.RECEIVED, msg[0]]);
-        switch (this.state.current) {
+        this._state.update([EMessageDirection.RECEIVED, msg[0]]);
+        switch (this._state.current) {
             case EConnectionState.CHALLENGING: {
                 const challengeMsg = msg as WampChallengeMessage;
 
-                if (!this.authProvider) {
-                    this.logger.log(LogLevel.ERROR, 'Received WAMP challenge, but no auth provider set.');
-                    this.transport.close(3000, 'Authentication failed.');
+                if (!this._authProvider) {
+                    this._logger.log(LogLevel.ERROR, 'Received WAMP challenge, but no auth provider set.');
+                    this._transport.close(3000, 'Authentication failed.');
                     return;
                 }
 
-                this.authProvider
+                this._authProvider
                     .computeChallenge(challengeMsg[2] || {})
                     .then((signature) => {
-                        if (!this.transport) {
+                        if (!this._transport) {
                             return;
                         }
-                        return this.transport.send([
+                        return this._transport.send([
                             EWampMessageID.AUTHENTICATE,
                             signature.signature,
                             signature.details || {},
                         ]);
                     })
                     .then(() => {
-                        this.state.update([
+                        this._state.update([
                             EMessageDirection.SENT,
                             EWampMessageID.AUTHENTICATE,
                         ]);
                     })
                     .catch((error) => {
-                        if (!this.transport) {
+                        if (!this._transport) {
                             return;
                         }
-                        this.logger.log(LogLevel.WARNING, 'Failed to compute challenge.', error);
-                        this.transport.close(3000, 'Authentication failed.');
+                        this._logger.log(LogLevel.WARNING, 'Failed to compute challenge.', error);
+                        this._transport.close(3000, 'Authentication failed.');
                     });
                 break;
             }
             case EConnectionState.ESTABLISHED: {
-                this.idGenerators = createIdGenerators();
-                this.processors = this.processorsFactories.map((procssorClass) => {
+                this._idGenerators = createIdGenerators();
+                this._processors = this._processorsFactories.map((procssorClass) => {
                     return new procssorClass(
-                        (msg) => this.transport!.send(msg),
+                        (msg) => this._transport!.send(msg),
                         (reason) => { this.handleProtocolViolation(reason); },
-                        this.idGenerators,
-                        this.logger,
+                        this._idGenerators,
+                        this._logger,
                     );
                 }) as any;
 
                 const [, sessionId, welcomeDetails] = msg as WampWelcomeMessage;
-                this.sessionId = sessionId;
-                this.logger.log(LogLevel.DEBUG, `Connection established.`, welcomeDetails);
+                this._sessionId = sessionId;
+                this._logger.log(LogLevel.DEBUG, `Connection established.`, welcomeDetails);
                 this.handleOnOpen(welcomeDetails);
                 break;
             }
             case EConnectionState.CLOSING: {
                 // - We received a GOODBYE message from the server, so we reply with goodbye and shutdown the transport.
-                this.transport.send([
+                this._transport.send([
                     EWampMessageID.GOODBYE,
                     { message: 'clean close' },
                     'wamp.close.goodbye_and_out',
                 ]);
-                this.state.update([EMessageDirection.SENT, EWampMessageID.GOODBYE]);
-                this.transport.close(1000, 'wamp.close.normal');
+                this._state.update([EMessageDirection.SENT, EWampMessageID.GOODBYE]);
+                this._transport.close(1000, 'wamp.close.normal');
                 break;
             }
             case EConnectionState.CLOSED: {
                 // - Clean close finished, actually close the transport, so `closed` and close callbacks will be created.
-                this.transport.close(1000, 'wamp.close.normal');
+                this._transport.close(1000, 'wamp.close.normal');
                 break;
             }
             case EConnectionState.ERROR: {
@@ -248,7 +261,7 @@ class Connection {
                 if (msg[0] !== EWampMessageID.ABORT) {
                     this.handleProtocolViolation('Protocol violation during session creation.');
                 } else {
-                    this.transport.close(3000, msg[2]);
+                    this._transport.close(3000, msg[2]);
                     this.handleOnOpen(new ConnectionOpenError(msg[2], msg[1]));
                 }
                 break;
@@ -258,35 +271,35 @@ class Connection {
 
     private processMessage(msg: WampMessage): void {
         if (msg[0] === EWampMessageID.GOODBYE) {
-            this.state.update([EMessageDirection.RECEIVED, msg[0]]);
+            this._state.update([EMessageDirection.RECEIVED, msg[0]]);
             return;
         }
 
-        const handled = this.processors!.some(
+        const handled = this._processors!.some(
             (processor) => processor.processMessage(msg),
         );
 
         if (!handled) {
-            this.logger.log(LogLevel.ERROR, `Unhandled message.`, msg);
+            this._logger.log(LogLevel.ERROR, `Unhandled message.`, msg);
             this.handleProtocolViolation('No handler found for message.');
         }
     }
 
     private sendHello(): void {
         const details: HelloMessageDetails = {
-            roles: Object.assign({}, ...this.processorsFactories.map(
+            roles: Object.assign({}, ...this._processorsFactories.map(
                 (processor) => processor.getFeatures(),
             )),
         };
 
-        if (this.authProvider) {
-            details.authid = this.authProvider.authId;
-            details.authmethods = [this.authProvider.authMethod];
+        if (this._authProvider) {
+            details.authid = this._authProvider.authId;
+            details.authmethods = [this._authProvider.authMethod];
         }
 
-        const message: WampHelloMessage = [EWampMessageID.HELLO, this.connectionOptions.realm, details];
-        this.transport!.send(message).then(
-            () => { this.state.update([EMessageDirection.SENT, EWampMessageID.HELLO]); },
+        const message: WampHelloMessage = [EWampMessageID.HELLO, this._options.realm, details];
+        this._transport!.send(message).then(
+            () => { this._state.update([EMessageDirection.SENT, EWampMessageID.HELLO]); },
             (err) => { this.handleProtocolViolation(`Transport error: ${err}.`); },
         );
     }
@@ -302,7 +315,7 @@ class Connection {
                 break;
             }
             case ETransportEventType.MESSAGE: {
-                if (this.state.current === EConnectionState.ESTABLISHED) {
+                if (this._state.current === EConnectionState.ESTABLISHED) {
                     this.processMessage(event.message);
                 } else {
                     this.processSessionMessage(event.message);
@@ -310,20 +323,20 @@ class Connection {
                 break;
             }
             case ETransportEventType.ERROR: {
-                this.logger.log(LogLevel.DEBUG, 'Connection error.', event.error);
-                if (this.state.current !== EConnectionState.ESTABLISHED) {
+                this._logger.log(LogLevel.DEBUG, 'Connection error.', event.error);
+                if (this._state.current !== EConnectionState.ESTABLISHED) {
                     this.handleOnOpen(new ConnectionOpenError(event.error));
                 }
                 break;
             }
             case ETransportEventType.CLOSE: {
-                this.logger.log(LogLevel.DEBUG, 'Connection closed.', event);
+                this._logger.log(LogLevel.DEBUG, 'Connection closed.', event);
 
-                this.transport = null;
-                this.state = new ConnectionStateMachine();
-                if (this.processors) {
-                    this.processors.forEach((processor) => processor.close());
-                    this.processors = null;
+                this._transport = null;
+                this._state = new ConnectionStateMachine();
+                if (this._processors) {
+                    this._processors.forEach((processor) => processor.close());
+                    this._processors = null;
                 }
 
                 if (!this.handleOnOpen(new ConnectionOpenError(event.reason))) {
@@ -339,8 +352,8 @@ class Connection {
     }
 
     private handleProtocolViolation(reason: WampURI): void {
-        if (!this.transport) {
-            this.logger.log(LogLevel.ERROR, 'Failed to handle protocol violation: Already closed.');
+        if (!this._transport) {
+            this._logger.log(LogLevel.ERROR, 'Failed to handle protocol violation: Already closed.');
             return;
         }
 
@@ -350,38 +363,38 @@ class Connection {
             'wamp.error.protocol_violation',
         ];
 
-        this.logger.log(LogLevel.ERROR, `Protocol violation: ${reason}.`);
-        this.transport.send(abortMessage);
-        this.transport.close(3000, 'protcol_violation');
+        this._logger.log(LogLevel.ERROR, `Protocol violation: ${reason}.`);
+        this._transport.send(abortMessage);
+        this._transport.close(3000, 'protcol_violation');
         this.handleOnOpen(new ConnectionOpenError('Protocol violation.'));
     }
 
     private handleOnOpen(details: Error | WelcomeDetails): boolean {
-        if (!this.openedDeferred) {
+        if (!this._openedDeferred) {
             return false;
         }
 
         if (details instanceof Error) {
-            this.openedDeferred.reject(details);
+            this._openedDeferred.reject(details);
         } else {
-            this.openedDeferred.resolve(details);
+            this._openedDeferred.resolve(details);
         }
-        this.openedDeferred = null;
+        this._openedDeferred = null;
 
         return true;
     }
 
     private handleOnClose(details: Error | ConnectionCloseInfo): void {
-        if (!this.closedDeferred) {
+        if (!this._closedDeferred) {
             return;
         }
 
         if (details instanceof Error) {
-            this.closedDeferred.reject(details);
+            this._closedDeferred.reject(details);
         } else {
-            this.closedDeferred.resolve(details);
+            this._closedDeferred.resolve(details);
         }
-        this.closedDeferred = null;
+        this._closedDeferred = null;
     }
 }
 
